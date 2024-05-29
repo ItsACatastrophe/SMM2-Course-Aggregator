00cc94d70474c9b4d80d846acfaeb50d2892ee9e
celltea <kitcatherine62@gmail.com>
2024-05-29 18:45:54 -0400

    Initial commit

---
+++ src/results.py
@@ -0,0 +1,63 @@
+import csv
+import os
+
+from course import Course
+
+# TODO: I think I need to rethink how files are treated by the program.
+# We write and read from the same file. Perhaps testing when data is saved
+# after writing is in order?
+# TODO: Rename this class, it does more than just writing, also reading and determining
+# new levels
+class Writer():
+    SEEN_PATH = './results/seen.csv'
+    SUPERBALL_COURSES_PATH = './results/superball_courses.csv'
+    
+    def __init__(self):
+        is_new_seen_file = not os.path.exists(Writer.SEEN_PATH)
+        is_new_superball_course_file = not os.path.exists(Writer.SUPERBALL_COURSES_PATH)
+        
+        if is_new_seen_file:
+            seen_file = open(Writer.SEEN_PATH, 'w+')
+            seen_writer = csv.DictWriter(seen_file, fieldnames=['course_code'])
+            seen_writer.writeheader()
+            seen_file.close()
+            
+        if is_new_superball_course_file:
+            superball_file = open(Writer.SUPERBALL_COURSES_PATH, 'w+')
+            superball_writer = csv.DictWriter(superball_file, fieldnames=Course.get_course_repr_keys())
+            superball_writer.writeheader()
+            superball_file.close()
+    
+    def get_unseen_courses(self, courses_info):
+        """
+        We should try to optimize this, making it use a generator
+        for seen codes is the first step.
+        """
+        seen_file = open(Writer.SEEN_PATH, 'r')
+        seen_codes = seen_file.read() # TODO: make this a generator, I worry for how large this file will get
+        
+        new_course_info = []   
+        for course_info in courses_info:
+            course_code = course_info.get("course_id")
+            
+            if not course_code in seen_codes:
+                new_course_info.append(course_info)
+                
+        return new_course_info
+    
+    def write_seen_m1_course_id(self, course_code):
+        self.seen_file = open(Writer.SEEN_PATH, 'a')
+        self.seen_writer = csv.DictWriter(self.seen_file, fieldnames=['course_code'])
+        self.seen_writer.writerow({'course_code': course_code})
+        
+        self.seen_file.close()
+    
+    def write_superball_course(self, course: Course):
+        self.superball_file = open(Writer.SUPERBALL_COURSES_PATH, 'a')
+        self.superball_writer = csv.DictWriter(self.superball_file, fieldnames=[*Course.get_course_repr_keys(), 'played'])
+        
+        course_summary = course.get_course_data() 
+        self.superball_writer.writerow(course.get_course_data())
+        
+        self.superball_file.close()
+        
---
+++ src/utils.py
@@ -0,0 +1,34 @@
+import constants
+from random import randint
+
+i_offset = randint(0,3)
+
+def seek_get(data, offset, size):
+    data.seek(offset)
+    return data.read(size)
+
+def print_status(print_text, count_superball_courses: int):
+    print(f"{constants.ERASE_LINE}[Found {count_superball_courses} Superballs] - {print_text}", end='')
+    
+def clear_decrypter_stdout():
+    """
+    Clears the stdout from the course decrypter program
+    Not very elegant but it'll do.
+    """
+    print(constants.LINE_UP, end=constants.LINE_CLEAR)
+    print(constants.LINE_UP, end=constants.LINE_CLEAR)
+    
+def get_difficulty(difficulty: str, loop: int):
+    if difficulty == '':
+        difficulty_index = (loop + i_offset) % len(constants.LEVEL_DIFFICULTIES)
+        difficulty = constants.LEVEL_DIFFICULTIES[difficulty_index]
+    
+    return difficulty
+
+# All internal course code usage format as FOOBARBAZ
+def format_course_code(code: str):
+    formatted_code = None
+    if code:
+        formatted_code = code.upper().replace('-', '')
+    return formatted_code
+        
